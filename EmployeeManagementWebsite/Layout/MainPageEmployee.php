e1eadb3d29eab29aa968410bdd82fcbe71675b79
Uzair-Hayat Choudhary <89047725+Uzair-HayatChoudhary@users.noreply.github.com>
2022-02-11 00:05:16 -0500

    Added project Skeleton
    added css file, templates for header/footer/navbar

---
+++ EmployeeManagementWebsite/Layout/MainPageEmployee.php
@@ -0,0 +1,12 @@
+<?php
+//Uzair is working on this!!!
+require_once '../Templates/Header.php';
+?>
+<div class="container-MainPageEmployee">
+    <?php
+    include '../Templates/Navbar.php';
+    ?>
+</div>
+<?php
+require_once '../Templates/Footer.php';
+?>
---
+++ EmployeeManagementWebsite/Templates/Navbar.php
@@ -0,0 +1,11 @@
+<?php
+//Uzair is working on this!!
+//All the content is there for nav bar, just need to fix styling and proper
+//proper colors depending on what we chose
+?>
+<div class="navbar">
+    <h1 id="navbar-Website-Title"><a href="../Layout/MainPageEmployee.php">EmPortal</a></h1>
+    <div id="navbar-Inbox"><a href="#">Inbox</a></div>
+    <div id="navbar-Absences"><a href="#">Absences & Requests</a></div>
+    <div id="navbar-Pay"><a href="#">Pay Info</a></div>
+</div>
